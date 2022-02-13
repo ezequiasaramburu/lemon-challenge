6793cff90032cd2751e331039a87a9a3900e82f5
Ezequias Aramburu <ezearamburu@hotmail.com>
2022-02-13 19:40:46 -0300

    update prettier config

--- src/screens/navigation/index.js
+++ src/screens/navigation/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from 'react-native';
 import Login from '../views/Login';
-import {Text} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
